acbb8c345ad9c7a50a77634538c75457155da479
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-10-08 23:56:02 +0300

    Create main.function.php

---
+++ Task_003/code/src/main.function.php
@@ -0,0 +1,41 @@
+<?php
+
+function main(string $configFileAddress) : string {
+    $config = readConfig($configFileAddress);
+
+    if(!$config){
+        return handleError("Невозможно подключить файл настроек");
+    }
+
+    $functionName = parseCommand();
+
+    if(function_exists($functionName)) {
+        $result = $functionName($config);
+    }
+    else {
+        $result = handleError("Вызываемая функция не существует");
+    }
+
+    return $result;
+}
+
+function parseCommand() : string {
+    $functionName = 'helpFunction';
+    
+    if(isset($_SERVER['argv'][1])) {
+        $functionName = match($_SERVER['argv'][1]) {
+            'read-all' => 'readAllFunction',
+            'add' => 'addFunction',
+            'search' => 'searchByBirthday',
+            'delete' => 'deleteRecord',
+            'export' => 'export',
+            'clear' => 'clearFunction',
+            'read-profiles' => 'readProfilesDirectory',
+            'read-profile' => 'readProfile',
+            'help' => 'helpFunction',
+            default => 'helpFunction'
+        };
+    }
+
+    return $functionName;
+}
