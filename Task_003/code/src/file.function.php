c9516905082c1ebcfd6d971b406fdb5fd802fd9d
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-10-08 23:55:14 +0300

    Create file.function.php

---
+++ Task_003/code/src/file.function.php
@@ -0,0 +1,119 @@
+<?php
+
+// function readAllFunction(string $address) : string {
+function readAllFunction(array $config) : string {
+    $address = $config['storage']['address'];
+    
+    if (file_exists($address) && is_readable($address)) {
+        $file = fopen($address, "rb");
+        
+        $contents = ''; 
+    
+        while (!feof($file)) {
+            $contents .= fread($file, 100);
+        }
+        
+        fclose($file);
+        return $contents;
+    }
+    else {
+        return handleError("Файл не существует");
+    }
+}
+
+// function addFunction(string $address) : string {
+function addFunction(array $config) : string {
+    $address = $config['storage']['address'];
+
+    $name = readline("Введите имя: ");
+    $date = readline("Введите дату рождения в формате ДД-ММ-ГГГГ: ");
+
+    if(validate($date)){
+        $data = $name . ", " . $date . "\r\n";
+
+        $fileHandler = fopen($address, 'a');
+        if(fwrite($fileHandler, $data)){
+            return "Запись $data добавлена в файл $address"; 
+        }
+        else {
+            return handleError("Произошла ошибка записи. Данные не сохранены");
+        }
+        fclose($fileHandler);
+    } else {
+        return handleError("Дата рождения указана в неправильном формате. Данные не сохранены");
+    }
+    
+}
+
+// function clearFunction(string $address) : string {
+function clearFunction(array $config) : string {
+    $address = $config['storage']['address'];
+
+    if (file_exists($address) && is_readable($address)) {
+        $file = fopen($address, "w");
+        
+        fwrite($file, '');
+        
+        fclose($file);
+        return "Файл очищен";
+    }
+    else {
+        return handleError("Файл не существует");
+    }
+}
+
+function helpFunction() {
+    return handleHelp();
+}
+
+function readConfig(string $configAddress): array|false{
+    return parse_ini_file($configAddress, true);
+}
+
+function readProfilesDirectory(array $config): string {
+    $profilesDirectoryAddress = $config['profiles']['address'];
+
+    if(!is_dir($profilesDirectoryAddress)){
+        mkdir($profilesDirectoryAddress);
+    }
+
+    $files = scandir($profilesDirectoryAddress);
+
+    $result = "";
+
+    if(count($files) > 2){
+        foreach($files as $file){
+            if(in_array($file, ['.', '..']))
+                continue;
+            
+            $result .= $file . "\r\n";
+        }
+    }
+    else {
+        $result .= "Директория пуста \r\n";
+    }
+
+    return $result;
+}
+
+function readProfile(array $config): string {
+    $profilesDirectoryAddress = $config['profiles']['address'];
+
+    if(!isset($_SERVER['argv'][2])){
+        return handleError("Не указан файл профиля");
+    }
+
+    $profileFileName = $profilesDirectoryAddress . $_SERVER['argv'][2] . ".json";
+
+    if(!file_exists($profileFileName)){
+        return handleError("Файл $profileFileName не существует");
+    }
+
+    $contentJson = file_get_contents($profileFileName);
+    $contentArray = json_decode($contentJson, true);
+
+    $info = "Имя: " . $contentArray['name'] . "\r\n";
+    $info .= "Фамилия: " . $contentArray['lastname'] . "\r\n";
+
+    return $info;
+}
