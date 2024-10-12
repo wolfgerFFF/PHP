15bd3f0d4cf1c99c2d5753c776a163b8fe44edda
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-10-12 22:31:33 +0300

    Create PageAboutModel.php

---
+++ Task_05/code/src/models/PageAboutModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\models;
+
+class PageAboutModel extends BaseModel
+{
+    public string $phone = '+7 (999) 999 99-99';
+    public string $address = 'г. Москва, ул. Придуманная 1';
+}
